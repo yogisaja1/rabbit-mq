decd895d17c056a5c6105b191b0cb42f89622547
= <harifin857@outlook.com>
2023-12-30 19:10:02 +0700

    Travelia Project

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+// Logo Preloader remove action
+const preloader = document.querySelector("[data-preloader]");
+window.addEventListener("load", () => {
+  preloader.classList.add("remove");
+});
+
+// add event on multiple elements
+
+const addEventOnElements = function (elements, evenType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(evenType, callback);
+  }
+};
+
+// Navbar toggler for mobile
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNav = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+};
+
+addEventOnElements(navTogglers, "click", toggleNav);
+
+// Header
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  header.classList[window.scrollY > 100 ? "add" : "remove"]("active");
+});
